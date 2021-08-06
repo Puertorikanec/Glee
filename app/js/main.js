c432a29110cb9173c82a1730a3468ab1f62d84a2
Puertorikanec <poblasov@yandex.ru>
2021-08-06 14:28:49 +0300

    добавлен слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,10 @@ $(document).ready(function(){
       $(".user-nav__inner").toggleClass("user-nav__inner--active");
     });
   });
+
+  $(function(){
+    $('.top-slider__inner').slick({
+      dots: true,
+      arrows: false
+  }) 
+})
